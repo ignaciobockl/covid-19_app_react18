bca9036a2473b1e02b2c1797c7ca83865e8a1b13
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-06-07 20:35:21 -0300

    Redux Essentials - RTK Query Basics -- useGetCountriesStatisticalDataQuery obtains the information of each country (Total cases, New cases, Total deaths, New deaths, Total Recovered, New recovered, Total Tests, New Tests, Active Cases, SeriousCritical, InfectionRisk, CaseFatalityRate, TestPercentage, etc)

--- src/services/covidApi.js
+++ src/services/covidApi.js
@@ -1,22 +1,44 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+/**
+    * ! Redux Essentials - RTK Query Basics
+    * ? https://redux.js.org/tutorials/essentials/part-7-rtk-query-basics
+    * ? https://redux.js.org/tutorials/essentials/part-8-rtk-query-advanced
+*/
 
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+/* -------------------- Environment Variables -------------------- */
 const baseUrl = process.env.REACT_APP_VACCOVID_BASE_URL;
 const rapidApiHost = process.env.REACT_APP_VACCOVID_X_RAPIDAPI_HOST;
 const rapidApiKey = process.env.REACT_APP_VACCOVID_X_RAPIDAPI_KEY;
+/* --------------------------------------------------------------- */
 
+// Headers necessary for the query to the API
 const covidApiHeaders = {
     'X-RapidAPI-Host': rapidApiHost,
 	'X-RapidAPI-Key': rapidApiKey 
 }
 
-const createRequest = ( url ) => ({ url, headers: covidApiHeaders });
+/* Creation of the createRequest constant to be able to indicate the url of the 
+    endpoint and the headers (in this case they are necessary to query the Api). */
+const createRequest = (url) => ({ url, headers: covidApiHeaders });
 
+// Define our single API slice object
 export const covidApi = createApi({
+    // The cache reducer expects to be added at `state.covidApi`
     reducerPath: 'covidApi',
+    // This request will have URLs starting with '/'
     baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoint: ( builder ) => ({
-        getCovidInfo: builder.query({
-            query: () => createRequest('/')
+    // The "endpoints" represent operations and requests for this server
+    endpoints: builder => ({
+        // The `getCountriesStatisticalData` endpoint is a "query" operation that returns data
+        getCountriesStatisticalData: builder.query({
+            // The URL for the request is '/'
+            query: () => createRequest()
         })
     })
-})
+});
+
+// Export the auto-generated hook for the `getCountriesStatisticalData` query endpoint
+export const { useGetCountriesStatisticalDataQuery } = covidApi;
