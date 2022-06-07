f3e6eb95d1a84e468d00650ac31b875e121f7650
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-06-07 19:48:38 -0300

    store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { covidApi } from "../services/covidApi";
+
 
 
 export default configureStore({
-    reducer: {},
-})
+    // Add the generated reducer as a specific top-level slice
+    reducer: {
+        [covidApi.reducerPath]: covidApi.reducer,
+    },
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(covidApi.middleware),
+});
