723935a594aacf3e48e3f336896d1e3fea0ef547
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-06-14 01:43:24 -0300

    Navbar Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,42 +4,35 @@ import './Navbar.css';
 
 
 
+const navbarOptions = [ 'ABOUT', 'ARTICLES', 'COVID-19', 'MAPS', 'NEWS', 'TREATMENT', 'VACCINE' ];
+
 export default function Navbar () {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">VACCOVID</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarScroll">
-                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">COVID-19</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">VACCINE</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">TREATMENT</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">MAP</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">NEWS</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">ARTICLES</a>
-                            </li>
-                            <li>
-                                <a className="nav-link active" aria-current="page" href="#">ABOUT</a>
-                            </li>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg vaccovid__navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand vaccovid__name" href="#">VACCOVID</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                        {
+                            navbarOptions.map((item, index) => (
+                                <li className="nav-item">
+                                    <a 
+                                        aria-current="page"
+                                        className="nav-link active"
+                                        href="#"
+                                        key={ index }
+                                    >
+                                        { item }
+                                    </a>
+                                </li>
+                            ))
+                        }
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     )
 }
