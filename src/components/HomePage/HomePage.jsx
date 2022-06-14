de2aee6e31147b158fc1a042293582b7381c4449
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-06-14 01:46:26 -0300

    Navbar Component

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+import './HomePage.css';
+
+
+
+export default function HomePage () {
+    return (
+        <div className="container-fluid div__margin">
+            <div className="row">
+                <div className="col">
+                    <Navbar />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+
+                </div>
+                <div className="col">
+
+                </div>
+            </div>
+        </div>
+    )
+}
